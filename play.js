f437c54594a04e400460575fe5caebe2703013da
Steph Nugs <steph.bowie1993@gmail.com>
2023-07-12 22:42:09 -0600

    refactored code using ES6 shorthand to export connect function

--- play.js
+++ play.js
@@ -1,19 +1,6 @@
-const net = require('net');
+const {connect} = require('./client');
 
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541
-  });
-
-  conn.on('data', (data) => {
-    console.log(data);
-  });
-
-  conn.setEncoding('utf8');
-
-  return conn;
-};
+connect;
 
 console.log('Connecting ...');
 connect();
